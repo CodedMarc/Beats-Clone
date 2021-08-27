169498685682e8e944e30e0f9fbe06f77d3ee990
Marc <73245838+CodedMarc@users.noreply.github.com>
2021-08-27 14:27:00 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navLinks = document.getElementsByClassName('nav-ul')[0];
+
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+})
+
+    gsap.to('.header-title-studio', {duration:1.5, ease: "power4.out", x:-1000});
+    gsap.to('.header-title-studio2', {duration:1.5, ease: "power4.out", x:-1000})
+    gsap.to("#studio-buds", {duration: 1.5, ease: "power2.out", x:1000 });
+
+    gsap.to('#beats-flex', {
+        scrollTrigger: {
+            trigger: '#studio-buds',
+            start: 'bottom center'
+        },
+        x:-1200
+    });
+    gsap.to('.second-title', {
+        scrollTrigger: {
+            trigger: '#beats-flex',
+            start: 'bottom center'
+        },
+        x:-1000
+    });
+    gsap.to('.second-title2', {
+        scrollTrigger: {
+            trigger: '#beats-flex',
+            start: 'bottom center'
+        },
+        x:-1000
+    });
+    gsap.to('#powerbeats', {
+        scrollTrigger: {
+            trigger: '#beats-flex',
+            start: 'bottom center'
+        },
+        x:-1000
+    });
+    gsap.to('#powerbeats-pro', {
+        scrollTrigger: {
+            trigger: '#powerbeats',
+            start: 'bottom center'
+        },
+        x:1000
+    });
+    const buyButton = document.querySelectorAll('.buyNow');
+    const learnButton = document.querySelectorAll('.learnMore');
+    buyButton.forEach(function(currentbtn) {
+        currentbtn.addEventListener('click', () => {
+            document.location.href = "https://www.beatsbydre.com/products";
+        })
+    })
+    learnButton.forEach(function(egg) {
+        egg.addEventListener('click', () => {
+            document.location.href = "https://www.beatsbydre.com/products";
+        })
+    })
